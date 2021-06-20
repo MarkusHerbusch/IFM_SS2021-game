7df75a01be4b5fabbae544cb18ee37d0f32d5103
Markus Herbusch <markus.herbusch@fh-bielefeld.de>
2021-06-20 22:24:22 +0200

    Framerate gesetzt

--- GameProgrammingSS2021/main.cpp
+++ GameProgrammingSS2021/main.cpp
@@ -2,8 +2,14 @@
 
 Game* game = nullptr;
 
-int main(int argc, const char * argv[])
+int main(int argc, const char* argv[])
 {
+
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
 
 	game = new Game();
 
@@ -24,10 +30,18 @@ int main(int argc, const char * argv[])
 
 	while (game->running()) {
 
+		frameStart = SDL_GetTicks();
+
 		game->handleEvents();
 		game->update();
 		game->render();
 
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	game->clean();
